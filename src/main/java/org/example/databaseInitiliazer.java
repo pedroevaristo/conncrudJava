723aab4213c40f3f77f888694f3e9b4d3b4f489f
pedroevaristo <pedrodzprogramacao@gmail.com>
2024-11-03 08:12:05 -0300

    criando conexão com o banco de dados e fazer o crud, usando arquitetura mvc

---
+++ src/main/java/org/example/databaseInitiliazer.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class databaseInitiliazer {
+
+    public static void main(String[] args) {
+        //refazer a conexão com banco de dados postgreSQL
+
+        
+    }
+}
